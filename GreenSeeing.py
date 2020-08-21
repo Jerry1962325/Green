224c3c15b227a802bdfd93990e7570777db3ccf5
Jerry1962325 <60921621+Jerry1962325@users.noreply.github.com>
2020-08-21 19:01:01 +0800

    Update GreenSeeing.py

--- GreenSeeing.py
+++ GreenSeeing.py
@@ -2,7 +2,8 @@ import cv2
 import numpy as np
 from tkinter import *
 from tkinter import filedialog
-
+#C=int(input("摄像头:(0为前置，1为来自USB)"))
+#M=input("识别模式:T为实时，F为图片")
 def colorTacing(cho,mod):
 	#默认识别绿色
     color_lower = np.array([20, 20,0])
@@ -24,7 +25,7 @@ def colorTacing(cho,mod):
         cv2.imshow('mask', mask)
         cv2.imshow('res', res)
         gpixel_count = (mask.reshape(-1, 1)[:, 0] != 0).sum()
-        print(str((gpixel_count/mask.size)*100)+"%")
+        print("绿视率:"+str((gpixel_count/mask.size)*100)+"%")
         if cv2.waitKey(5) & 0xFF == 27:
             break
     if mod=="F":
@@ -36,13 +37,18 @@ def colorTacing(cho,mod):
         mask = cv2.erode(mask, None, iterations=2)
         mask = cv2.GaussianBlur(mask, (3, 3), 0)
         res = cv2.bitwise_and(frame, frame, mask=mask)
+        cv2.imshow('frame', frame)
+        cv2.imshow('mask', mask)
+        cv2.imshow('res', res)
         gpixel_count = (mask.reshape(-1, 1)[:, 0] != 0).sum()
-        G = str((gpixel_count/mask.size)*100)+"%"
-        return G
-    cap.release()
+        print("绿视率:" + str((gpixel_count / mask.size) * 100) + "%")
+        cv2.waitKey(5) & 0xFF == 27
+        input()
+        cap.release()
     cv2.destroyAllWindows()
 
-#colorTacing(0,"T",'./3.jpg')
+##colorTacing(C,M)
+
 
 window = Tk()
 window.title("GreenSeeing")
@@ -52,8 +58,8 @@ r1 = Radiobutton(window, text='使用前置摄像头',variable=var1, value=0).pa
 r2 = Radiobutton(window, text='使用USB摄像头', variable=var1, value=1).pack()
 r3 = Radiobutton(window, text='捕捉模式', variable=var2, value='T').pack()
 r4 = Radiobutton(window, text='文件模式', variable=var2, value='F').pack()
-L = Label(window,text='绿视率').pack()
 def Get():print(colorTacing(var1.get(), var2.get()))
 B=Button(window,text='开始',command=Get).pack()
 window.mainloop()
+
 
